bea99051adbfb089a51ae7cca33af07470291e37
Conan <xukenan@vsan.cn>
2021-12-16 14:08:55 +0800

    Initial commit

---
+++ src/main/java/top/xukenan/mybootdemo/MyBootDemoApplication.java
@@ -0,0 +1,13 @@
+package top.xukenan.mybootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBootDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/xukenan/mybootdemo/MyBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.xukenan.mybootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
